3c2082a436832fd2c44bc6ec1826222f9f24301a
Marx_Mas <116768281+LuisReynoso233531@users.noreply.github.com>
2024-10-23 21:19:45 -0700

    cambios

--- Entities/appointment.js
+++ Entities/appointment.js
@@ -1,5 +1,3 @@
-const pool = require('../Database/db');
-
 class Appointment {
     constructor(id = null, date, hours, place, description) {
         this.id = id;
@@ -7,39 +5,6 @@ class Appointment {
         this.hours = hours;
         this.place = place;
         this.description = description;
-    }
-
-    static async create(date, hours, place, description) {
-        const [result] = await pool.query(
-            'INSERT INTO appointment (date, hours, place, description) VALUES (?, ?, ?, ?)',
-            [date, hours, place, description]
-        );
-        return result.insertId;
-    }
-
-    static async read(id) {
-        const [rows] = await pool.query('SELECT * FROM appointment WHERE id = ?', [id]);
-        if (rows.length > 0) {
-            const { id, date, hours, place, description } = rows[0];
-            return new Appointment(id, date, hours, place, description);
-        }
-        return null;
-    }
-
-    static async update(id, date, hours, place, description) {
-        await pool.query(
-            'UPDATE appointment SET date = ?, hours = ?, place = ?, description = ? WHERE id = ?',
-            [date, hours, place, description, id]
-        );
-    }
-
-    static async delete(id) {
-        await pool.query('DELETE FROM appointment WHERE id = ?', [id]);
-    }
-
-    static async findAll() {
-        const [rows] = await pool.query('SELECT * FROM appointment');
-        return rows.map(row => new Appointment(row.id, row.date, row.hours, row.place, row.description));
     }
 }
 
